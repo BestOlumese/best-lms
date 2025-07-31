8df4cb409c3a3b69cd802a49436cba624e1b96eb
Amiolemen Best <bestolumese@gmail.com>
2025-07-31 11:43:48 +0100

    02: Auth & Security

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,30 @@
 import { betterAuth } from "better-auth";
- 
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { env } from "./env";
+import { resend } from "./resend";
+import { emailOTP } from "better-auth/plugins";
+
 export const auth = betterAuth({
-  //...
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        await resend.emails.send({
+          from: "BestLMS <onboarding@resend.dev>",
+          to: [email],
+          subject: "BestLMS - Verify your email",
+          html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+        });
+      },
+    }),
+  ],
 });
